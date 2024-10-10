99c0b45592cd6c34bcb0c02c0aea3c094f74205e
Ivan Romero <i.wells.ar@gmail.com>
2024-10-10 10:36:51 -0600

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from http.server import BaseHTTPRequestHandler
+import pymupdf4llm
+import requests
+import os
 
+file_url = "https://egzfmwstzbapteywkqqq.supabase.co/storage/v1/object/public/techme_documents/181f9e334c-6ffa-4b31-80b2-9ac5028d9ffb.pdf"
 
-class handler(BaseHTTPRequestHandler):
+response = requests.get(file_url)
+file_buffer = response.content
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+with open("file.pdf", "wb") as f:
+    f.write(file_buffer)
+
+md_text = pymupdf4llm.to_markdown("file.pdf")
+os.remove("file.pdf")
+print(md_text)
